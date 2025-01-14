29a519b976db818979500d080657a817c8b5c817
SAMORJII <samueluzoma440@gmai.com>
2025-01-14 03:35:00 -0800

    All My Programs

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("Hello world, welcome to java");
+		System.out.println("Learning \rJava is fun");
+		System.out.print("Hurray , \" I did it\"");
+		
+		int x = 15;
+		
+		System.out.printf("The value of x is %d: ",x);
+	}
+}	
---
+++ TenaryOperator.java
@@ -0,0 +1,7 @@
+public class TernaryOperator{
+	public static void main(String[] args) {
+		int age = 25;
+		
+		String result(age >= 18) ? "You are Qualified":"You are not Qualified";
+	}
+}
