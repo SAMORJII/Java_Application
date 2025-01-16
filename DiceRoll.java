11380a172961ce5b0e4b715ece5597678c63c69f
SAMORJII <samueluzoma440@gmai.com>
2025-01-16 02:59:54 -0800

    All my programs

---
+++ DiceRoll.java
@@ -0,0 +1,11 @@
+import java.security.SecureRandom;
+
+public class DiceRoll{
+	public static void main(String[] args){
+		SecureRandom randomNumbers = new SecureRandom();
+		
+		int dieRoll = randomNumbers.nextInt(6) + 1;
+		
+		System.out.printf("You rolled %d", + dieRoll);
+	}
+}
